036b4eed55931afe4c73f651add4b6938323ab46
rdrew <robdrewupei@gmail.com>
2015-02-10 14:30:56 -0400

    1st commit

---
+++ theme-settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Theme settings file for the bowing down home theme.
+ */
+
+require_once dirname(__FILE__) . '/template.php';
+
+/**
+ * Implements hook_form_FORM_alter().
+ */
+function bowing_down_home_form_system_theme_settings_alter(&$form, $form_state) {
+  // You can use this hook to append your own theme settings to the theme
+  // settings form for your subtheme. You should also take a look at the
+  // 'extensions' concept in the Omega base theme.
+}
