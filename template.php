7d1e308eaa15349dcd9bd01d373bfe4d341febd8
Rob Drew <rdrew@libprojects.upei.ca>
2015-02-25 15:11:54 -0400

    added media queries

--- template.php
+++ template.php
@@ -5,3 +5,12 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * bowing down home theme.
  */
+function bowing_down_home_form_alter(&$form, &$form_state, $form_id) {
+   // target a single form
+  if($form_id == "islandora_solr_simple_search_form"){
+    $form['simple']['islandora_simple_search_query']['#default_value'] = t('Search:'); // Set a default value for the textfield
+  $form['simple']['islandora_simple_search_query']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search:';}";
+    $form['simple']['islandora_simple_search_query']['#attributes']['onfocus'] = "if (this.value == 'Search:') {this.value = '';}";
+  }
+
+} 
